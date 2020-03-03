114d60842ea3f1187186f56c0dc5fba27c1d2331
Ali-man <aliman.fsd@gmail.com>
2020-03-03 13:24:04 +0500

    Revert "Added basic template for example"
    This reverts commit 26c8f8b2

--- apps/general/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from apps.general.models import ConfigSite, Slider
-
-admin.site.register(Slider)
-admin.site.register(ConfigSite)
--- apps/general/context_processors.py
+++
@@ -1,6 +0,0 @@
-from apps.products.models import Category
-
-
-def general_context(request):
-    categories = Category.objects.all().order_by('sort')
-    return {'categories': categories}
